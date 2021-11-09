aee2514adf562da8fe01716fa5f8862e8c9d2285
sc1entist <the-scientist@rootstorm.com>
2021-11-08 18:25:58 -0700

    Add crypto wallets

--- elfread.c
+++ elfread.c
@@ -1,6 +1,17 @@
 /* elfread.c
  * the-scientist@rootstorm.com
  * spl0its-r-us security
+ * 
+ * 100% of the proceeds that wind up in these accounts will be donated to animal shelters.  
+ * ================================================================================================================
+ * BITCOIN: bc1qc0x6qdsk7auhsrym6vz0rtafnl2qgqjk7yy3tn
+ * ETHEREUM: 0x482d85E39Ce865Dcf7c26bFDD6e52AB203d0f555
+ * DOGECOIN: DPYxWnnyYzmPYWP92iqo4DizJht3rZnYnu
+ * LITECOIN: ltc1qea6ehaanwr9q3jygmw75q35avk8t74h7sc5uc3
+ * ETHCLASSIC: 0x6C63D4428Cb6BfDB7AC72b447A8B29D811395052
+ * CARDANO: addr1qxn4przua2crcrgwt3pk5465ym3syytfn2v7gssu7ayuvpvefqwdvkgzn4y3j5d5ynsh03kae9k8d0z8yuh8excuv6xqdl4kyt
+ * ================================================================================================================
+ *
  */
 
  /*
