11653af60328582eab5ceae1c991a3e02b89202e
sc1entist <the-scientist@rootstorm.com>
2021-11-09 13:29:49 -0700

    add machine ELF (haha) also add elf type

--- elfread.c
+++ elfread.c
@@ -44,6 +44,7 @@ main(int argc, char** argv)
         size_t datasz;
         Elf64_Ehdr ehdr;
         Elf64_Half e_type;
+        Elf64_Word e_version;
         bool status;
 
         const char* elf_class[ELFCLASSNUM] = {
@@ -89,13 +90,193 @@ main(int argc, char** argv)
                 "Processor-specific range"
         };
 
+        const char* elf_e_machine[EM_NUM] = {
+                "No machine",
+                "AT&T WE 32100",
+                "SUN SPARC",
+                "Intel 80386",
+                "Motorola m68k family",
+                "Motorola m88k family",
+                "Intel MCU",
+                "Intel 80860",
+                "MIPS R3000 big-endian",
+                "IBM System/370",
+                "MIPS R3000 little-endian",
+                "reserved","reserved","reserved","reserved",
+                "HPPA",
+                "reserved",
+                "Fujitsu VPP500",
+                "Sun's v8plus",
+                "Intel 80960",
+                "PowerPC","PowerPC 64-bit",
+                "IBM S390","IBM SPU/SPC",
+                "reserved","reserved","reserved","reserved","reserved",
+                "reserved","reserved","reserved","reserved","reserved",
+                "reserved","reserved",
+                "NEC V800 series",
+                "Fujitsu FR20",
+                "TRW RH-32",
+                "Motorola RCE",
+                "ARM",
+                "Digital Alpha",
+                "Hitachi SH",
+                "SPARC v9 64-bit",
+                "Siemens Tricore",
+                "Argonaut RISC Core",
+                "Hitachi H8/300","Hitachi H8/300H","Hitachi H8S","Hitachi H8/500",
+                "Intel Merced",
+                "Stanford MIPS-X",
+                "Motorola Coldfire","Motorola M68HC12",
+                "Fujitsu MMA Multimedia Accelerator",
+                "Siemens PCP",
+                "Sony nCPU embeeded RISC",
+                "Denso NDR1 microprocessor",
+                "Motorola Start*Core processor",
+                "Toyota ME16 processor",
+                "STMicroelectronic ST100 processor",
+                "Advanced Logic Corp. Tinyj emb.fam",
+                "AMD x86-64 architecture",
+                "Sony DSP Processor",
+                "Digital PDP-10","Digital PDP-11",
+                "Siemens FX66 microcontroller",
+                "STMicroelectronics ST9+ 8/16 mc","STmicroelectronics ST7 8 bit mc",
+                "Motorola MC68HC16 microcontroller","Motorola MC68HC11 microcontroller","Motorola MC68HC08 microcontroller","Motorola MC68HC05 microcontroller",
+                "Silicon Graphics SVx",
+                "STMicroelectronics ST19 8 bit mc",
+                "Digital VAX",
+                "Axis Communications 32-bit emb.proc",
+                "Infineon Technologies 32-bit emb.proc",
+                "Element 14 64-bit DSP Processor",
+                "LSI Logic 16-bit DSP Processor",
+                "Donald Knuth's educational 64-bit proc",
+                "Harvard University machine-independent object files",
+                "SiTera Prism",
+                "Atmel AVR 8-bit microcontroller",
+                "Fujitsu FR30",
+                "Mitsubishi D10V","Mitsubishi D30V",
+                "NEC v850",
+                "Mitsubishi M32R","Matsushita MN10300","Matsushita MN10200",
+                "picoJava",
+                "OpenRISC 32-bit embedded processor",
+                "ARC International ARCompact",
+                "Tensilica Xtensa Architecture",
+                "Alphamosaic VideoCore",
+                "Thompson Multimedia General Purpose Proc",
+                "National Semi. 32000",
+                "Tenor Network TPC",
+                "Trebia SNP 1000",
+                "STMicroelectronics ST200",
+                "Ubicom IP2xxx",
+                "MAX processor",
+                "National Semi. CompactRISC",
+                "Fujitsu F2MC16",
+                "Texas Instruments msp430",
+                "Analog Devices Blackfin DSP",
+                "Seiko Epson S1C33 family",
+                "Sharp embedded microprocessor",
+                "Arca RISC",
+                "PKU-Unity & MPRC Peking Uni. mc series",
+                "eXcess configurable cpu",
+                "Icera Semi. Deep Execution Processor",
+                "Altera Nios II",
+                "National Semi. CompactRISC CRX",
+                "Motorola XGATE",
+                "Infineon C16x/XC16x",
+                "Renesas M16C",
+                "Microchip Technology dsPIC30F",
+                "Freescale Communication Engine RISC",
+                "Renesas M32C",
+                "reserved","reserved","reserved","reserved","reserved",
+                "reserved","reserved","reserved","reserved","reserved",
+                "Altium TSK3000",
+                "Freescale RS08",
+                "Analog Devices SHARC family",
+                "Cyan Technology eCOG2",
+                "Sunplus S+core7 RISC",
+                "New Japan Radio (NJR) 24-bit DSP",
+                "Broadcom VideoCore III",
+                "RISC for Lattice FPGA",
+                "Seiko Epson C17",
+                "Texas Instruments TMS320C6000 DSP","Texas Instruments TMS320C2000 DSP","Texas Instruments TMS320C55x DSP",
+                "Texas Instruments App. Specific RISC","Texas Instruments Prog. Realtime Unit",
+                "reserved","reserved","reserved","reserved","reserved",
+                "reserved","reserved","reserved","reserved","reserved",
+                "reserved","reserved","reserved","reserved","reserved",
+                "STMicroelectronics 64bit VLIW DSP",
+                "Cypress M8C",
+                "Renesas R32C",
+                "NXP Semi. TriMedia",
+                "QUALCOMM DSP6",
+                "Intel 8051 and variants",
+                "STMicroelectronics STxP7x",
+                "Andes Tech. compact code emb. RISC",
+                "Cyan Technology eCOG1X",
+                "Dallas Semi. MAXQ30 mc",
+                "New Japan Radio (NJR) 16-bit DSP",
+                "M2000 Reconfigurable RISC",
+                "Cray NV2 vector architecture",
+                "Renesas RX",
+                "Imagination Tech. META",
+                "MCST Elbrus",
+                "Cyan Technology eCOG16",
+                "National Semi. CompactRISC CR16",
+                "Freescale Extended Time Processing Unit",
+                "Infineon Tech. SLE9X",
+                "Intel L10M","Intel K10M",
+                "reserved",
+                "ARM AARCH64",
+                "reserved",
+                "Amtel 32-bit microprocessor",
+                "STMicroelectronics STM8",
+                "Tileta TILE64","Tilera TILEPro",
+                "Xilinx MicroBlaze",
+                "NVIDIA CUDA",
+                "Tilera TILE-Gx",
+                "CloudShield",
+                "KIPO-KAIST Core-A 1st gen.","KIPO-KAIST Core-A 2nd gen.",
+                "Synopsys ARCv2 ISA. ",
+                "Open8 RISC",
+                "Renesas RL78",
+                "Broadcom VideoCore V",
+                "Renesas 78KOR",
+                "Freescale 56800EX DSC",
+                "Beyond BA1","Beyond BA2",
+                "XMOS xCORE",
+                "Microchip 8-bit PIC(r)",
+                "reserved", "reserved", "reserved", "reserved", "reserved",
+                "KM211 KM32","KM211 KMX32","KM211 KMX16","KM211 KMX8","KM211 KVARC",
+                "Paneve CDP",
+                "Cognitive Smart Memory Processor",
+                "Bluechip CoolEngine",
+                "Nanoradio Optimized RISC",
+                "CSR Kalimba",
+                "Zilog Z80",
+                "Controls and Data Services VISIUMcore",
+                "FTDI Chip FT32",
+                "Moxie processor",
+                "AMD GPU",
+                "reserved","reserved","reserved","reserved","reserved",
+                "reserved","reserved","reserved","reserved","reserved",
+                "reserved","reserved","reserved","reserved","reserved",
+                "reserved","reserved","reserved",
+                "RISC-V",
+                "unspecified","unspecified","unspecified",
+                "Linux BPF -- in-kernel virtual machine",
+                "unspecified","unspecified","unspecified","unspecified",
+                "C-SKY"
+        };
+
+        const char* elf_e_version[EV_NUM] = {
+                "Invalid ELF version",
+                "Current version"
+        };
+
         read_file_into_mem("/bin/ls", (void**)&data, &datasz);
         memcpy(&ehdr, data, sizeof(Elf64_Ehdr));
         if (strncmp(ELFMAG, &ehdr.e_ident[EI_MAG0], SELFMAG) != 0)
                 err_exit("* Not an ELFMAG");
 
         status = true;
-        ehdr.e_ident[EI_CLASS] = -1; 
         if (ehdr.e_ident[EI_CLASS] < ELFCLASS32 || ehdr.e_ident[EI_CLASS] > ELFCLASS64) {
                 status = false;
                 ehdr.e_ident[EI_CLASS] = ELFCLASSNONE;
@@ -124,6 +305,8 @@ main(int argc, char** argv)
         else if (e_type >= ET_HIOS && e_type <= ET_LOPROC)
                 e_type = INDEX_ET_PROC;
 
+        e_version = ehdr.e_version != EV_CURRENT ? EV_NONE : ehdr.e_version;
+
         printf(
                 "ELF Header:\n"
                 "  Magic:   "
@@ -138,8 +321,8 @@ main(int argc, char** argv)
                 "  OS/ABI:                              %s\n"
                 "  ABI Version:                         %d\n"
                 "  Type:                                %s\n"
-                "  Machine:                             \n"
-                "  Version:                             \n"
+                "  Machine:                             %s\n"
+                "  Version:                             0x%.8x(%s)\n"
                 "  Entry point address:                 \n"
                 "  Start of program headers:            \n"
                 "  Start of section headers:            \n"
@@ -159,7 +342,9 @@ main(int argc, char** argv)
                                              // Linux kernel (after at least 2.6) has no definition of it,
                                              // so it is ignored for statically-linked executables.
                                              // In that case, offset and size of EI_PAD are 8.
-                elf_e_type[e_type]
+                elf_e_type[e_type],
+                ehdr.e_machine >= EM_NUM ? "special\n" : elf_e_machine[ehdr.e_machine],
+                ehdr.e_version, elf_e_version[e_version]
         );
 
         free(data);
